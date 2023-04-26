747c45b05a924f3af1dfc879e5367dd79b639d81
hardi kotadiya <hardikotadiya080@gmail.com>
2023-04-26 22:10:32 +0530

    hardi

--- lib/res/constant/images.dart
+++ lib/res/constant/images.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 class AppImages {
   static const String imagePath = "assets/images/";
 
--- lib/res/constant/strings.dart
+++ lib/res/constant/strings.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 class AppStrings {
   //
   static const String Searchrecipesingredients = "Search recipes, ingredients";
